61fd76a1290033c8d666f7a10f4a25a9b93f35b6
finteger <nashassociatesinc@gmail.com>
2024-04-10 15:19:01 -0600

    update

--- lib/screens/my_home_page.dart
+++ lib/screens/my_home_page.dart
@@ -39,10 +39,10 @@ class _MyHomePageState extends State<MyHomePage> {
                 height: 300,
                 color: Colors.white,
                 child: Padding(
-                  padding: const EdgeInsets.all(18.0),
+                  padding: const EdgeInsets.all(12.0),
                   child: TableCalendar(
                     calendarFormat: CalendarFormat.month,
-                    headerVisible: false,
+                    headerVisible: true,
                     focusedDay: DateTime.now(),
                     firstDay: DateTime(2023),
                     lastDay: DateTime(2025),
