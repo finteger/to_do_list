3f34886627bf7d9b4caddef9f0ce7cbfe96dfea4
finteger <nashassociatesinc@gmail.com>
2024-04-15 15:31:24 -0600

    update

--- lib/screens/my_home_page.dart
+++ lib/screens/my_home_page.dart
@@ -77,6 +77,71 @@ class _MyHomePageState extends State<MyHomePage> {
                 ),
               ),
             ),
+            Expanded(
+              child: ShaderMask(
+                shaderCallback: (Rect bounds) {
+                  return LinearGradient(
+                    begin: Alignment(0.00, 0.9999),
+                    end: Alignment(0.00, 0.22),
+                    colors: <Color>[
+                      Colors.white.withOpacity(0.9),
+                      Colors.transparent,
+                    ],
+                  ).createShader(bounds);
+                },
+                blendMode: BlendMode.dstOut,
+                child: ListView.builder(
+                  padding: const EdgeInsets.all(1),
+                  itemCount: tasks.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Container(
+                      decoration: BoxDecoration(
+                        color: checkboxes[index]
+                            ? Colors.green.withOpacity(0.7)
+                            : Colors.blue.withOpacity(0.7),
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      margin: EdgeInsets.all(2),
+                      child: Row(
+                        children: [
+                          Expanded(
+                            child: Padding(
+                              padding: const EdgeInsets.all(18.0),
+                              child: Row(
+                                children: [
+                                  Icon(
+                                    !checkboxes[index]
+                                        ? Icons.manage_history
+                                        : Icons.playlist_add_check_circle,
+                                    color: !checkboxes[index]
+                                        ? Colors.white
+                                        : Colors.white,
+                                    size: 32,
+                                  ),
+                                  SizedBox(width: 18),
+                                  Text(
+                                    '${tasks[index]}',
+                                    style: checkboxes[index]
+                                        ? TextStyle(
+                                            decoration:
+                                                TextDecoration.lineThrough,
+                                            fontSize: 20,
+                                            color:
+                                                Colors.black.withOpacity(0.5),
+                                          )
+                                        : TextStyle(fontSize: 20),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ),
             Padding(
               padding: EdgeInsets.all(20.0),
               child: Row(
